3fefff5075c36ec1bdbe303590063d7ba36c94c6
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-01-30 22:27:26 -0500

    feat: set up session storage

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 // App imports
-const express = require("express");
-const path = require("node:path");
 require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const path = require("node:path");
+const pgSession = require("connect-pg-simple")(session);
+const pool = require("./pool");
+const passport = require("passport");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
+require("./config/passport");
 
 // App constants
 const PORT = process.env.PORT || 8080;
 const assetsPath = path.join(__dirname, "public");
+const sessionStore = new pgSession({
+  pool: pool,
+});
 
 // App initializations
 const app = express();
@@ -16,6 +26,35 @@ app.set("view engine", "ejs");
 // App level middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(assetsPath));
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+    secret: "temp",
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+  })
+);
+app.use(passport.session());
+app.use((req, res, next) => {
+  console.log(req.session);
+  console.log(req.user);
+  next();
+});
+
+// Routes
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+app.get("/sign-up", (req, res, next) => {
+  res.render("sign-up");
+});
+app.get("/log-in", (req, res, next) => {
+  res.render("log-in");
+});
 
 // App server
 app.listen(PORT, () => {
