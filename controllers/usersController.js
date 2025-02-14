1222222a7dafe08746e884de6966868f6a6609e1
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-02-14 18:13:06 -0500

    feat: add form validation for user sign up

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcryptjs");
+const { validateUser } = require("../validation/validateUser");
+const { validationResult } = require("express-validator");
 
 // Prisma client initialization
 const prisma = new PrismaClient();
@@ -16,22 +18,31 @@ module.exports.logOut = (req, res, next) => {
 };
 
 // POST middlewares
-module.exports.createUser = async (req, res, next) => {
-  const { firstName, lastName, email, password } = req.body;
-  bcrypt.hash(password, 10, async (err, hashedPassword) => {
-    if (err) {
-      return next(err);
-    } else {
-      await prisma.user.create({
-        data: {
-          firstName,
-          lastName,
-          email,
-          password: hashedPassword,
-        },
+module.exports.createUser = [
+  validateUser,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    //console.log(errors.array());
+    //console.log(typeof errors.array());
+    if (errors.isEmpty()) {
+      const { firstName, lastName, email, password } = req.body;
+      bcrypt.hash(password, 10, async (err, hashedPassword) => {
+        if (err) {
+          return next(err);
+        } else {
+          await prisma.user.create({
+            data: {
+              firstName,
+              lastName,
+              email,
+              password: hashedPassword,
+            },
+          });
+          res.redirect("/log-in");
+        }
       });
-      res.redirect("/log-in");
+    } else {
+      return res.status(400).render("sign-up", { errors: errors.array() });
     }
-  });
-  //res.redirect("/");
-};
+  },
+];
---
+++ validation/validateUser.js
@@ -0,0 +1,85 @@
+const { body } = require("express-validator");
+const { PrismaClient } = require("@prisma/client");
+
+// Constants
+const blankMsg = "cannot be left blank.";
+const passwordMsg = "Password must contain at least ";
+const DIGITS = "1234567890";
+
+// Prisma client initialization
+const prisma = new PrismaClient();
+
+// Validation chain for user sign-up
+module.exports.validateUser = [
+  body("firstName")
+    .trim()
+    .notEmpty()
+    .withMessage("First name " + blankMsg)
+    .bail(),
+  body("lastName")
+    .trim()
+    .notEmpty()
+    .withMessage("Last name " + blankMsg)
+    .bail(),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email " + blankMsg)
+    .bail()
+    .isEmail()
+    .withMessage("Must be a valid email address.")
+    .bail()
+    // Check if email is already in user database
+    .custom(async (value) => {
+      const emailInUse = await prisma.user.findUnique({
+        where: {
+          email: value,
+        },
+      });
+      if (emailInUse) {
+        throw new Error(
+          "This email is already in use. Please sign up using a different email address."
+        );
+      } else return true;
+    })
+    .bail(),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password " + blankMsg)
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least six characters long.")
+    .bail()
+    // Check if password contains at least one uppercase letter
+    .custom(async (value) => {
+      const hasUpperCase = value !== value.toLowerCase();
+      if (!hasUpperCase) {
+        throw new Error(passwordMsg + "one uppercase letter.");
+      } else return true;
+    })
+    .bail()
+    // Check if password contains at least one lowercase letter
+    .custom(async (value) => {
+      const hasLowerCase = value !== value.toUpperCase();
+      if (!hasLowerCase) {
+        throw new Error(passwordMsg + "one lowercase letter.");
+      } else return true;
+    })
+    .bail()
+    // Check if password contains at least three numbers letter
+    .custom(async (value) => {
+      let numCount = 0;
+      const chars = value.split("");
+      //console.log(chars);
+      chars.forEach((char) => {
+        if (DIGITS.includes(char)) {
+          numCount++;
+        }
+      });
+      if (numCount < 3) {
+        throw new Error(passwordMsg + "three numbers.");
+      } else return true;
+    })
+    .bail(),
+];
